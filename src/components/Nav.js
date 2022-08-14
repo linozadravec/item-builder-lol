9201e3347049499619e321ffcdba023435c0f7cf
linozadravec <lzadravec@foi.hr>
2022-08-14 20:09:54 +0200

    Added donation button
    Added unstylized donation button that opens a Ko-fi link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,22 @@ import React from 'react'
 import '../css/navbar.css'
 
 export default function Nav(){
+
+    function openInNewTab(url){
+        window.open(url, '_blank', 'noopener,noreferrer');
+    }
+
     return (
         <nav className="navMenu">
+            <div>
             <a href="#play" >Play</a>
             <a href="#about" >About</a>
             <div className="dot"></div>
+            </div>
+            <div>
+            <button className="donationBtn" onClick={() => openInNewTab("https://ko-fi.com/linodev")}> Buy me a Teemo skin</button>
+            </div>
+            
         </nav>
     )
 }
